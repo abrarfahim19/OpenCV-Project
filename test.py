333e4e7f68bb1d6ba2dca6ea51d4def576f07d37
abrarfahim19 <fahimpatwari19@gmail.com>
2021-12-10 22:14:04 +0600

    Stacked

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
 import numpy as np
-
 import cv2 as cv
 
-print( cv.__version__ )
-print('Hello World')
+##########################################
+import utils
+
+path = "1.jpg"
+widthImg = 700
+heightImg = 700
+##########################################
+
+img = cv.imread(path)
+img = cv.resize(img,(widthImg,heightImg))
+
+imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+imgBlur = cv.GaussianBlur(imgGray,(5,5),1)
+imgCanny = cv.Canny(imgBlur,10,50)
+
+imgArray = ([img,imgGray,imgBlur,imgCanny])
+imgStacked = utils.stackImages(imgArray,0.5)
+
+cv.imshow('Stacked',imgStacked)
+cv.waitKey(0)
