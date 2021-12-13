187b827a9573ea7433becfa25f168a7d09b06cbe
abrarfahim19 <fahimpatwari19@gmail.com>
2021-12-13 23:29:58 +0600

    Wraped

--- test.py
+++ test.py
@@ -25,17 +25,25 @@ cv.drawContours(imgContour,contours,-1,(0,255,0),10)
 #   Find Rectangles     ##################
 rectContour = utils.rectContour(contours)
 markContourPoints = utils.getCornerPoints(rectContour[0])
-markContourPoints = utils.reorder(markContourPoints)
 gradePoints = utils.getCornerPoints(rectContour[1])
-gradePoints = utils.reorder(gradePoints)
 
 if markContourPoints.size != 0 and gradePoints.size != 0:
     cv.drawContours(imgMarkContour,markContourPoints,-1,(0,0,255),20)
     cv.drawContours(imgMarkContour,gradePoints,-1,(255,0,0),20)
 
+    markContourPoints = utils.reorder(markContourPoints)  # Reorder
+    gradePoints = utils.reorder(gradePoints)  # Reorder
+    pt1 = np.float32(markContourPoints)
+    imgPoint = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+    matrix = cv.getPerspectiveTransform(pt1, imgPoint)
+    imgWrapColored = cv.warpPerspective(img, matrix, (widthImg, heightImg))
+
+#   Wraping The Image   ##################
+
+
 #   Stacked Images      ##################
 imgBlank = np.zeros_like(img)
-imgArray = ([img,imgGray,imgBlur,imgCanny],[imgContour,imgMarkContour,imgBlank,imgBlank])
+imgArray = ([img,imgGray,imgBlur,imgCanny],[imgContour,imgMarkContour,imgWrapColored,imgBlank])
 imgStacked = utils.stackImages(imgArray,0.5)
 
 cv.imshow('Stacked',imgStacked)
